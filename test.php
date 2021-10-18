11721de30905c93b42081ab6efb4e409414e94cc
franknjomo <njomonf@3il.fr>
2021-10-18 08:48:15 +0200

    Formatage

--- test.php
+++ test.php
@@ -1,12 +1,14 @@
 <?php
-if(isset($_POST['btn']))
-$sexe=$_POST["sexe"];
-$maConnexion = new PDO('mysql:host=localhost;dbname=reservation','root','');
-$var=$maConnexion->prepare('INSERT INTO client (SexeC)  VALUES (:sexe)');
-$var->bindValue(':sexe',$sexe);
-$var->execute();
+    if (isset($_POST['btn']))
+        $sexe = $_POST["sexe"];
+
+    $maConnexion = new PDO('mysql:host=localhost;dbname=reservation', 'root', '');
+    $var = $maConnexion->prepare('INSERT INTO client (SexeC)  VALUES (:sexe)');
+    $var->bindValue(':sexe', $sexe);
+    $var->execute();
 ?>
-<form method="POST" >
+
+<form method="POST">
     <label>Sexe</label>
     <input type="text" name="sexe">
     <button type="submit" class="btn btn-primary" name="btn">Creer mon compte</button>
