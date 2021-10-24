06cfbdf1085d0c209187989476dd374379b8439e
Wilfried Nguegang <ngueganw@3il.fr>
2021-10-24 19:00:05 +0200

    xldm

--- creation.php
+++ creation.php
@@ -15,6 +15,15 @@
 </head>
 
 <body>
+
+<style>
+body {
+  background-image: url('front/image/enregistrement_client.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
     <?php
     if(isset($_POST['btn']))
     $email=$_POST['email'];
@@ -45,39 +54,39 @@
             <div class="row">
                 <div class="col-md-6 offset-3">
                     <form class="needs-validation" novalidate method="POST" >
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label>Adresse e-mail</label>
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Saisissez votre @ mail*">
+                            <input type="mail" class="form-control" id="email" name="email" placeholder="Saisissez votre @ mail*">
 
                         </div>
-                        <div class="col-3 mr-auto ">
+                        <div class="col-md-6 mr-auto ">
                             <label>Mot de passe</label>
                             <input type="password" class="form-control" id="password" name="password">
                         </div>
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label for="Sexe">Sexe</label>
                             <select class="form-control" name="sexe">
                             <option value="Féminin">Féminin</option>
                             <option value="Masculin">Masculin</option>
                         </select>
                         </div>
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label for="Nom">Nom</label>
                             <input type="text" class="form-control" id="nom" name="nom" placeholder="Saisissez votre nom*">
                         </div>
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label>Prénom</label>
                             <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Saisissez votre prénom*">
                         </div>
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label>Ville</label>
                             <input type="text" class="form-control" id="ville" name="ville" placeholder="Saisissez votre ville">
                         </div>
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label>Numéro de téléphone</label>
                             <input type="text" class="form-control" id="numero" name="numero" placeholder="Saisissez votre numéro portable">
                         </div>
-                        <div class="col-3 mr-auto">
+                        <div class="col-md-6 mr-auto">
                             <label>Date de naissance</label>
                             <input type="date" class="form-control" id="date" name="date" placeholder="Saisissez votre date de naissance">
                         </div>
@@ -86,7 +95,7 @@
                             <input type="checkbox" class="form-check-input" id="exampleCheck1">
                             <label class="form-check-label" for="exampleCheck1">J'accepte les conditions d'utilisation</label>
                         </div><br>
-                        <button type="submit" class="btn btn-primary" name="btn">Creer mon compte</button>
+                        <button type="submit" class="btn btn-primary" name="btn">Je Crée mon compte</button>
                     </form>
                 </div>
 
--- reservation.php
+++ reservation.php
@@ -7,15 +7,23 @@
   @$dateA=$_POST["dateA"];
   @$dateF=$_POST["dateF"];
   @$telephone=$_POST["telephone"];
-  $maConnexion = new PDO('mysql:host=localhost;dbname=reservation','root','');
-  $var=$maConnexion->prepare('INSERT INTO reservation ( DateA, DateD, Email, Nom,Prenom,Telephone)  VALUES (:dateA,:dateF,:email,:nom,:prenom,:telephone)');
-  @$var->bindValue(':email',$email);
-  @$var->bindValue('prenom',$prenom);
-  $var->bindValue('nom',$nom);
-  $var->bindValue('dateA',$dateA);
-  $var->bindValue('dateF',$dateF);
-  $var->bindValue('telephone',$telephone);
-  $var->execute();}
+      if(!empty($email) && !empty($nom)  && !empty($dateA) && !empty($dateF) && !empty($prenom)){
+    $maConnexion = new PDO('mysql:host=localhost;dbname=reservation','root','');
+    $var=$maConnexion->prepare('INSERT INTO reservation ( DateA, DateD, Email, Nom,Prenom,Telephone)  VALUES (:dateA,:dateF,:email,:nom,:prenom,:telephone)');
+    @$var->bindValue(':email',$email);
+    @$var->bindValue('prenom',$prenom);
+    $var->bindValue('nom',$nom);
+    $var->bindValue('dateA',$dateA);
+    $var->bindValue('dateF',$dateF);
+    $var->bindValue('telephone',$telephone);
+    $var->execute();
+        }
+         else{
+          echo "Renseigner toutes les informations manquantes";
+        }
+                          }
+
   if($var->execute()){
-  echo "Enregistrement validé";}
+  echo "Enregistrement validé";
+                     }
 ?>
